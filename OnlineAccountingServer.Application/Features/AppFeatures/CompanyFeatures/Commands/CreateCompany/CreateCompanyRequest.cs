755b639cc94a21d8d5e00b5c48c7cd3e64eb4563
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-07-06 23:19:26 +0300

    cqrs pattern

---
+++ OnlineAccountingServer.Application/Features/AppFeatures/CompanyFeatures/Commands/CreateCompany/CreateCompanyRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAccountingServer.Application.Features.AppFeatures.CompanyFeatures.Commands.CreateCompany
+{
+    public sealed class CreateCompanyRequest
+    {
+    }
+}
