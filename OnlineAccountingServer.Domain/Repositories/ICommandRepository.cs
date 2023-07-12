4649b792bff838d43104f343c9a52400a302c69f
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-07-12 14:59:17 +0300

    repo pattern

--- OnlineAccountingServer.Domain/Repositories/ICommandRepository.cs
+++ OnlineAccountingServer.Domain/Repositories/ICommandRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineAccountingServer.Domain.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace OnlineAccountingServer.Domain.Repositories
 {
-    public interface ICommandRepository
+    public interface ICommandRepository<T>
+        where T : Entity
     {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        Task RemoveByI(string id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
